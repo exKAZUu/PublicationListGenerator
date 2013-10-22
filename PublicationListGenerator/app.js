402b2a148600dc7601b12dbf6fd283cd3363b27f
Kazunori SAKAMOTO <exkazuu@gmail.com>
2013-10-22 14:23:10 +0900

    Adding template feature.

--- PublicationListGenerator/app.js
+++ PublicationListGenerator/app.js
@@ -1,55 +1,133 @@
-﻿/// <reference path="jquery.d.ts" />
+/// <reference path="jquery.d.ts" />
 /// <reference path="sugar.d.ts" />
+/// <reference path="Scripts/linq.d.ts" />
+function normalize(text) {
+    if (text.startsWith('"') && text.endsWith('"')) {
+        text = text.substring(1, text.length - 1);
+    }
+    text = text.trim().replace('\r', ' ').replace('\n', ' ').replace('  ', ' ').replace('  ', ' ').replace('  ', ' ');
+    return text;
+}
+
 var Author = (function () {
-    function Author() { }
+    function Author(item) {
+        var items = item.trim().split(' ');
+        this.firstName = items[0];
+        this.lastName = items[1];
+    }
+    Author.prototype.equals = function (name) {
+        return name.replace(' ', '') == this.firstName + this.lastName;
+    };
+
+    Author.prototype.shortName = function () {
+        var reg = new RegExp('[a-zA-Z]');
+        if (this.firstName.has(reg) || this.lastName.has(reg)) {
+            return this.firstName[0] + '. ' + this.lastName;
+        } else {
+            return this.firstName + this.lastName;
+        }
+    };
+
+    Author.prototype.fullName = function () {
+        var reg = new RegExp('[a-zA-Z]');
+        if (this.firstName.has(reg) || this.lastName.has(reg)) {
+            return this.firstName + ' ' + this.lastName;
+        } else {
+            return this.firstName + this.lastName;
+        }
+    };
     return Author;
 })();
-var Conference = (function () {
-    function Conference() { }
-    return Conference;
+
+var Award = (function () {
+    function Award(item) {
+        var items = item.trim().split(' ');
+        this.organization = items[0];
+        this.name = items[1];
+    }
+    return Award;
 })();
+
+var Publication = (function () {
+    function Publication(line) {
+        var items = line.split('\t').map(normalize);
+        var i = 0;
+        this.authors = items[i++].split(",").map(function (item) {
+            return new Author(item);
+        });
+        this.title = items[i++];
+        this.conference = items[i++];
+        this.abbr = items[i++];
+        this.volume = items[i++];
+        this.page = items[i++];
+        if (this.page.has('-')) {
+            this.page = 'pp. ' + this.page;
+        }
+        this.year = parseInt(items[i++]);
+        this.month = parseInt(items[i++]);
+        this.location = items[i++];
+        this.awards = items[i++].split(',').map(function (item) {
+            return new Award(item);
+        });
+        this.refereed = items[i++] != null;
+    }
+    return Publication;
+})();
+
 var PublicationListGenerator = (function () {
     function PublicationListGenerator() {
     }
-    PublicationListGenerator.prototype.normalize = function (text) {
-        if(text.startsWith('"') && text.endsWith('"')) {
-            text = text.substring(1, text.length - 1);
-        }
-        text = text.replace("\r", " ").replace("\n", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ");
-        return text;
-    };
     PublicationListGenerator.prototype.generate = function (csv) {
-        var _this = this;
         var lines = csv.split('\n');
         return lines.map(function (line) {
-            var items = line.split('\t').map(_this.normalize);
-            var pub = new Conference();
-            var i = 0;
-            pub.authors = items[i++];
-            pub.title = items[i++];
-            pub.conference = items[i++];
-            pub.abbr = items[i++];
-            pub.location = items[i++];
-            pub.page = items[i++];
-            pub.year = parseInt(items[i++]);
-            pub.month = parseInt(items[i++]);
-            pub.refereed = items[i++] == '—L';
-            return pub;
+            return new Publication(line);
         });
     };
     return PublicationListGenerator;
 })();
+
 window.onload = function () {
-    var input = document.getElementById("input");
-    var output = document.getElementById("output");
-    var button = document.getElementById("generate");
+    var inputElem = document.getElementById("input");
+    var authorElem = document.getElementById("author");
+    var fullTemplateElem = document.getElementById("fullTemplate");
+    var shortTemplateElem = document.getElementById("shortTemplate");
+    var outputElem = document.getElementById("output");
+    var buttonElem = document.getElementById("generate");
+
+    var fullTemplate = fullTemplateElem.value;
+    var shortTemplate = shortTemplateElem.value;
+    var authorNames = Enumerable.from(authorElem.value.split('\n')).select(function (name) {
+        return normalize(name);
+    });
+
     var gen = new PublicationListGenerator();
-    button.addEventListener('click', function () {
-        var pubs = gen.generate(input.value);
+    buttonElem.addEventListener('click', function () {
+        var pubs = gen.generate(inputElem.value);
         var pubText = pubs.map(function (pub) {
-            return pub.authors + ', "' + pub.title + '," ' + pub.conference + ' (' + pub.abbr + '), ' + pub.location + ', ' + 'pp.' + pub.page + ', ' + pub.year + '. ' + pub.month + '.';
-        }).join('');
-        output.value = pubText;
+            var authorIndex = Enumerable.from(pub.authors).indexOf(function (author) {
+                return authorNames.any(function (n) {
+                    return author.equals(n);
+                });
+            });
+            var result = "";
+            if (authorIndex >= 0 && authorIndex < pub.authors.length - 1 && shortTemplate.length > 0) {
+                return 'short' + result;
+            } else {
+                result = fullTemplate;
+                result = result.replace('%authors%', pub.authors.map(function (a) {
+                    return a.shortName();
+                }).join(', '));
+                result = result.replace('%title%', pub.title);
+                result = result.replace('%conference%', pub.conference);
+                if (pub.abbr != undefined && pub.abbr.length > 0) {
+                    result += ' (' + pub.abbr + ')';
+                }
+                result += ', ' + pub.page;
+                result += ', ' + pub.year + '.';
+                return 'full' + result;
+            }
+        }).join('\n');
+        outputElem.value = pubText;
     });
 };
-//@ sourceMappingURL=app.js.map
+//# sourceMappingURL=app.js.map
--- PublicationListGenerator/app.ts
+++ PublicationListGenerator/app.ts
@@ -1,71 +1,145 @@
 /// <reference path="jquery.d.ts" />
 /// <reference path="sugar.d.ts" />
+/// <reference path="Scripts/linq.d.ts" />
 
-class Author {
-    firstName: string;
-    lastName: string;
+function normalize(text: string) {
+    if (text.startsWith('"') && text.endsWith('"')) {
+        text = text.substring(1, text.length - 1)
+    }
+    text = text.trim()
+        .replace('\r', ' ')
+        .replace('\n', ' ')
+        .replace('  ', ' ')
+        .replace('  ', ' ')
+        .replace('  ', ' ')
+    return text
 }
 
-class Conference {
-    authors: string;
-    title: string;
-    conference: string;
-    abbr: string;
-    location: string;
-    page: string;
-    year: number;
-    month: number;
-    refereed: bool;
+class Author {
+    firstName: string
+    lastName: string
+
+    constructor(item: string) {
+        var items = item.trim().split(' ')
+        this.firstName = items[0]
+        this.lastName = items[1]
+    }
+
+    equals(name: string) {
+        return name.replace(' ', '') == this.firstName + this.lastName
+    }
+
+    shortName() {
+        var reg = new RegExp('[a-zA-Z]')
+        if (this.firstName.has(reg) || this.lastName.has(reg)) {
+            return this.firstName[0] + '. ' + this.lastName
+        } else {
+            return this.firstName + this.lastName
+        }
+    }
+
+    fullName() {
+        var reg = new RegExp('[a-zA-Z]')
+        if (this.firstName.has(reg) || this.lastName.has(reg)) {
+            return this.firstName + ' ' + this.lastName
+        } else {
+            return this.firstName + this.lastName
+        }
+    }
+}
+
+class Award {
+    organization: string
+    name: string
+
+    constructor(item: string) {
+        var items = item.trim().split(' ')
+        this.organization = items[0]
+        this.name = items[1]
+    }
+}
+
+class Publication {
+    authors: Author[]
+    title: string
+    conference: string
+    abbr: string
+    volume: string
+    page: string
+    year: number
+    month: number
+    location: string
+    awards: Award[]
+    refereed: boolean
+
+    constructor(line: string) {
+        var items = line.split('\t')
+            .map(normalize)
+        var i = 0
+        this.authors = items[i++].split(",").map(item => new Author(item))
+        this.title = items[i++]
+        this.conference = items[i++]
+        this.abbr = items[i++]
+        this.volume = items[i++]
+        this.page = items[i++]
+        if (this.page.has('-')) {
+            this.page = 'pp. ' + this.page
+        }
+        this.year = parseInt(items[i++])
+        this.month = parseInt(items[i++])
+        this.location = items[i++]
+        this.awards = items[i++].split(',').map(item => new Award(item))
+        this.refereed = items[i++] != null
+    }
 }
 
 class PublicationListGenerator {
     constructor() {
     }
 
-    normalize(text: string) {
-        if (text.startsWith('"') && text.endsWith('"')) {
-            text = text.substring(1, text.length - 1);
-        }
-        text = text.replace("\r", " ")
-            .replace("\n", " ")
-            .replace("  ", " ")
-            .replace("  ", " ")
-            .replace("  ", " ");
-        return text;
-    }
-
     generate(csv: string) {
-        var lines = csv.split('\n');
-        return lines.map((line: string) => {
-            var items = line.split('\t')
-                .map(this.normalize);
-            var pub = new Conference();
-            var i = 0;
-            pub.authors = items[i++];
-            pub.title = items[i++];
-            pub.conference = items[i++];
-            pub.abbr = items[i++];
-            pub.location = items[i++];
-            pub.page = items[i++];
-            pub.year = parseInt(items[i++]);
-            pub.month = parseInt(items[i++]);
-            pub.refereed = items[i++] == '—L';
-            return pub;
-        });
+        var lines = csv.split('\n')
+        return lines.map(line => new Publication(line))
     }
 }
 
 window.onload = () => {
-    var input = <HTMLTextAreaElement>document.getElementById("input");
-    var output = <HTMLTextAreaElement>document.getElementById("output");
-    var button = <HTMLButtonElement>document.getElementById("generate");
-    var gen = new PublicationListGenerator();
-    button.addEventListener('click', () => {
-        var pubs = gen.generate(input.value);
+    var inputElem = <HTMLTextAreaElement>document.getElementById("input")
+    var authorElem = <HTMLTextAreaElement>document.getElementById("author")
+    var fullTemplateElem = <HTMLTextAreaElement>document.getElementById("fullTemplate")
+    var shortTemplateElem = <HTMLTextAreaElement>document.getElementById("shortTemplate")
+    var outputElem = <HTMLTextAreaElement>document.getElementById("output")
+    var buttonElem = <HTMLButtonElement>document.getElementById("generate")
+
+    var fullTemplate = fullTemplateElem.value
+    var shortTemplate = shortTemplateElem.value
+    var authorNames = Enumerable
+        .from(authorElem.value.split('\n'))
+        .select(name => normalize(name))
+
+    var gen = new PublicationListGenerator()
+    buttonElem.addEventListener('click', () => {
+        var pubs = gen.generate(inputElem.value)
         var pubText = pubs.map((pub) => {
-            return pub.authors + ', "' + pub.title + '," ' + pub.conference + ' (' + pub.abbr + '), '
-                + pub.location + ', ' + 'pp.' + pub.page + ', ' + pub.year + '. ' + pub.month + '.';
-        }).join('');
-        output.value = pubText;
-    });
+            var authorIndex = Enumerable.from(pub.authors)
+                .indexOf(author => authorNames.any(n => author.equals(n)))
+            var result = ""
+            if (authorIndex >= 0 && authorIndex < pub.authors.length - 1 && shortTemplate.length > 0) {
+                return 'short' + result
+            }
+            else {
+                result = fullTemplate
+                result = result.replace('%authors%', pub.authors.map(a => a.shortName()).join(', '))
+                result = result.replace('%title%', pub.title)
+                result = result.replace('%conference%', pub.conference)
+                if (pub.abbr != undefined && pub.abbr.length > 0) {
+                    result += ' (' + pub.abbr + ')'
+                }
+                result += ', ' + pub.page
+                result += ', ' + pub.year + '.'
+                return 'full' + result
+            }
+        }).join('\n')
+        outputElem.value = pubText
+    })
 }
